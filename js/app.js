75ccc5ba2dfd2ba08276cedc0094186168bc2947
mangalambigai <mangalambigais@gmail.com>
2016-05-03 08:41:57 -0400

    Add angular timer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('mathApp', ['ngRoute', 'ui.bootstrap'])
+angular.module('mathApp', ['ngRoute', 'ui.bootstrap', 'timer'])
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider
     .when('/quiz', {
